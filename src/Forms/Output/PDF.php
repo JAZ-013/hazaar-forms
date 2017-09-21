4b2ff32768c3839b0989fa9eb4f18937a946abed
Jamie Carl <jamie@hazaarlabs.com>
2017-09-21 10:05:25 +1000

    Tweaks to the form CSS renderer
    We are now loading this from a file which can be updated more easily.

--- src/Forms/Output/PDF.php
+++ src/Forms/Output/PDF.php
@@ -45,52 +45,10 @@ class PDF extends HTML {
 
     private function renderStyle(){
 
-        $style = 'body {
-            font-family: Tahoma, Geneva, sans-serif;
-        }
-        h2, h3 { margin: 0 0 15px 0; }
-        .form-header { margin-bottom: 50px; }
-        .form-header .form-logo { float: right; max-height: 125px; }
-        .well {
-            background: #eee;
-            padding: 25px;
-            margin-bottom: 25px;
-            float: left;
-            width: 100%;
-        }
-        .row {
-            float: left;
-            width: 100%;
-        }
-        .col-md-1,
-        .col-md-2,
-        .col-md-3,
-        .col-md-4,
-        .col-md-5,
-        .col-md-6,
-        .col-md-7,
-        .col-md-8,
-        .col-md-9,
-        .col-md-10,
-        .col-md-11,
-        .col-md-12 {
-            float: left;
-        }
-        .col-md-1  { width: 8.33333% }
-        .col-md-2  { width: 16.66667% }
-        .col-md-3  { width: 25% }
-        .col-md-4  { width: 33.33333% }
-        .col-md-5  { width: 41.66667% }
-        .col-md-6  { width: 50% }
-        .col-md-7  { width: 58.33333% }
-        .col-md-8  { width: 66.66667% }
-        .col-md-9  { width: 75% }
-        .col-md-10 { width: 83.33333% }
-        .col-md-11 { width: 91.66667% }
-        .col-md-12 { width: 100% }
-        ';
+        if($file = \Hazaar\Loader::getModuleFilePath('pdf.css'))
+            return file_get_contents($file);
 
-        return $style;
+        return null;
 
     }
 
