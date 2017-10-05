e731404ee5900a3c093b20c84cde9bf99e914fcf
Jamie Carl <jamie@hazaarlabs.com>
2017-10-05 13:47:59 +1100

    Custom validation and more
    * Most of the validation stuff is now in a child element in the field definition called "validate".
    * Custom validation is available using validate.custom
    * Tweaked the existing validation options to mirror what was already available in the Hazaar\Strict\Model class.  This means that both frontend and backend validation is active at the same time.  The only option that is unsupported by the backend is "custom" as this uses JavaScript.

--- libs/interact.js
+++ libs/interact.js
@@ -437,8 +437,8 @@
             return {
                 done: function (callback) {
                     var result = _validate_field(host, name);
-                    if (result === true && 'api' in def) {
-                        _post(host, 'api', { "target": def.api, "params": { "name": name, "value": value, "def": def } }, false).done(function (response) {
+                    if (result === true && 'validate' in def && 'api' in def.validate) {
+                        _post(host, 'api', { "target": def.validate.api, "params": { "name": name, "value": value, "def": def } }, false).done(function (response) {
                             var result = (response.ok === true) ? true : { "field": name, "status": response.reason || "api_failed(" + def.api + ")" };
                             callback(result);
                         });
@@ -455,25 +455,45 @@
         }
         if ('required' in def && !value)
             return { "field": name, "status": "required" };
-        if ('min' in def) {
-            if (def.type == 'array' && value.length < def.min)
-                return { "field": name, "status": "too_few" };
-            else if ((def.type == 'int' || def.type == 'integer' || def.type == 'number') && parseInt(value) < def.min)
-                return { "field": name, "status": "too_small" };
-            else if (def.type === 'text' && value.length < def.min)
-                return { "field": name, "status": "too_short" };
-        }
-        if ('max' in def) {
-            if (def.type == 'array' && value.length > def.max)
-                return { "field": name, "status": "too_many" };
-            else if ((def.type == 'int' || def.type == 'integer' || def.type == 'number') && parseInt(value) > def.max)
-                return { "field": name, "status": "too_big" };
-            else if (def.type === 'text' && value.length > def.max)
-                return { "field": name, "status": "too_long" };
-        }
         if ('format' in def) {
             if (!Inputmask.isValid(String(value), def.format))
                 return { "field": name, "status": "bad_format", "format": def.format };
+        }
+        if ('validate' in def) {
+            for (type in def.validate) {
+                var data = def.validate[type];
+                switch (type) {
+                    case 'min':
+                        if (parseInt(value) < data)
+                            return { "field": name, "status": "too_small" };
+                        break;
+                    case 'max':
+                        if (parseInt(value) > data)
+                            return { "field": name, "status": "too_big" };
+                        break;
+                    case 'with':
+                        var reg = new RegExp(data);
+                        if (!value.match(reg))
+                            return { "field": name, "status": "regex_failed", "pattern": data };
+                        break;
+                    case 'equals':
+                        if (value !== data)
+                            return { "field": name, "status": "not_equal" };
+                        break;
+                    case 'minlen':
+                        if (value.length < data)
+                            return { "field": name, "status": "too_short" };
+                        break;
+                    case 'maxlen':
+                        if (value.length > data)
+                            return { "field": name, "status": "too_long" };
+                        break;
+                    case 'custom':
+                        if (!_eval(host, data))
+                            return { "field": name, "status": "custom" };
+                        break;
+                }
+            }
         }
         if ('valid' in def)
             return def.valid;
