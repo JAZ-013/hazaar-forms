2e5494fb148c488079543c31b61240e745d3ed2f
Jamie Carl <jamie@hazaarlabs.com>
2017-10-17 15:31:04 +1100

    Added dataBinder value tags to html output fields

--- libs/interact.js
+++ libs/interact.js
@@ -504,8 +504,12 @@
         } else {
             field = $('<div>');
         }
-        if ('html' in def)
-            field.append(def.html);
+        if ('html' in def) {
+            var html = def.html;
+            while (match = html.match(/\{\{(\w+)\}\}/))
+                html = html.replace(match[0], '<span data-bind="' + [match[1]] + '"></span>');
+            field.append(html);
+        }
         if ('show' in def) {
             if (typeof def.show == 'boolean')
                 field.toggle(def.show);
