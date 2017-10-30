e1a0519e854fdd55c389c4b86daa1b34be12be46
Jamie Carl <jamie@hazaarlabs.com>
2017-10-30 11:28:59 +1100

    Added extra output class attribute to field def
    Fixes #33

--- src/Forms/Output/HTML.php
+++ src/Forms/Output/HTML.php
@@ -192,6 +192,9 @@ class HTML extends \Hazaar\Forms\Output {
         if($html = ake($field, 'html'))
             $group->add($this->model->matchReplace((string)$html, true));
 
+        if($class = ake(ake($field, 'output'), 'class'))
+            $group->addClass($class);
+
         return $group;
 
     }
