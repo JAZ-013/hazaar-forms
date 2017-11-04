f902a965bdfa091c6362459d0a271f531f616169
Jamie Carl <jamie@hazaarlabs.com>
2017-11-04 21:46:11 +1100

    Added support for setting title in PDF output

--- src/Controller/Form.php
+++ src/Controller/Form.php
@@ -166,6 +166,8 @@ abstract class Form extends Action {
             if(!($name = $this->request->get('name')))
                 throw new \Exception('No form name specified!');
 
+            $params = ($this->request->has('params') ? unserialize($this->request->params) : array());
+
             $this->form($name);
 
             $this->model->populate($this->load(unserialize($this->request->get('params'))));
@@ -185,6 +187,8 @@ abstract class Form extends Action {
                 $response = new \Hazaar\Controller\Response\PDF();
 
                 $response->setContent($output->render());
+
+                $response->setTitle($this->model->getTitle($params));
 
             }
 
