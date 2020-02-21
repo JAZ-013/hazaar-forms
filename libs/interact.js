8cdd5e885099b0da9a4bc7a61f323e46f49e27a9
Jamie Carl <jamie@hazaarlabs.com>
2020-02-21 16:07:32 +1100

    Phase one: Working with static options
    Need to get it working with API options.  Which may be problematic. :(

--- libs/interact.js
+++ libs/interact.js
@@ -2051,10 +2051,16 @@ dataBinderArray.prototype.diff = function (data, callback) {
         }).fail(_error);
     }
 
-    function _fix_booleans(data) {
+    function _fix_plain_data(host, data, p) {
         for (x in data) {
-            if (data[x] !== null && typeof data[x] === 'object' && !('__hz_value' in data[x])) _fix_booleans(data[x]);
+            let key = (p ? p + '.' : '') + x, def = null;
+            if (data[x] !== null && typeof data[x] === 'object' && !('__hz_value' in data[x])) _fix_plain_data(host, data[x], key);
             else if (typeof data[x] === 'boolean') data[x] = { '__hz_value': data[x], '__hz_label': data[x] ? 'Yes' : 'No' };
+            else if ((def = _form_field_lookup(host.def, key)) !== null && 'options' in def) {
+                _input_options(host, def, null, null, function (s, options) {
+                    data[x] = { '__hz_value': data[x], '__hz_label': options[data[x]] };
+                });
+            }
         }
         return data;
     }
@@ -2063,7 +2069,7 @@ dataBinderArray.prototype.diff = function (data, callback) {
         let p = function (response) {
             if (!response.ok) return;
             if ('horizontal' in host.def) host.settings.horizontal = host.def.horizontal;
-            host.data.extend(_fix_booleans(response.form));
+            host.data.extend(_fix_plain_data(host, response.form));
             host.data.commit();
             $(host).trigger('data', [host.data.save()]);
             _nav(host, 0);
