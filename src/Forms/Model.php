9deb72c09a80fe52c79b34aa47d389f58e9b1aa6
Jamie Carl <jamie@hazaar.io>
2020-10-30 17:23:19 +1100

    Reverted default API method to POST

--- src/Forms/Model.php
+++ src/Forms/Model.php
@@ -1415,7 +1415,7 @@ class Model extends \Hazaar\Model\Strict {
 
             $router = new \Hazaar\Application\Router(new \Hazaar\Application\Config);
 
-            $request = new \Hazaar\Application\Request\Http($params, false, ake($args, 'method', 'GET'));
+            $request = new \Hazaar\Application\Request\Http($params, false, ake($args, 'method', 'POST'));
 
             if($pos = strpos($target, '?')){
 
