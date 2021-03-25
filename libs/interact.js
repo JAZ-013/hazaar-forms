8e010471643bdbb3786abf43e36a60bf046fac54
Jamie Carl <jamie@hazaar.io>
2021-03-25 13:16:59 +1100

    Added fake hzForm that outputs an "unsupported" error
    This error is shown when a browser is not supported.

--- libs/interact.js
+++ libs/interact.js
@@ -153,6 +153,22 @@ dataBinderArray.prototype.diff = function (data, callback) {
 
 (function ($) {
 
+    if(typeof Symbol !== 'function'){
+
+        $.fn.hzForm = function () {
+            let msg = 'Your browser is too old and does not support ES2015.  Please update to the latest version of your browser.';
+            if (window.navigator.userAgent.indexOf("MSIE ") >= 0) msg = [
+                'Internet Explorer is not supported. See ',
+                $('<a target="_blank">').attr('href', 'https://www.microsoft.com/en-au/microsoft-365/windows/end-of-ie-support').html('here'),
+                ' for details on how to upgrade.'
+            ];
+            $(this).html($('<div class="alert alert-danger text-center">').html(msg));
+        }
+
+        return;
+
+    }
+
     //Error capture method
     function _error(error) {
         if (typeof error === 'string') error = { str: error };
